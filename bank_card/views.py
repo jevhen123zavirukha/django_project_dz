ffc7b84a930a671445f9d653ec064779bbcc721e
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-08-06 08:56:00 +0200

    add function validate_credit_card\views.py\bank_card

--- bank_card/views.py
+++ bank_card/views.py
@@ -1,1 +1,14 @@
+# Task 2
+import re
+from django.http import HttpResponse
+
+
+def validate_credit_card(request):
+    card_number = input('Enter your card number: (Must be in format - 9999-9999-9999-9999): ')
+    pattern = re.compile(r"^\d{4}-\d{4}-\d{4}-\d{4}$")
+
+    if not pattern.match(card_number):
+        return HttpResponse('<h4>Invalid card number<h4>')
+    else:
+        return HttpResponse('<h4>Your card number is valid<h4>')
 
