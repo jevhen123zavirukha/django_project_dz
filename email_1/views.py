763dc2e5806dd49fbc9a5c7a76582b9c6da07664
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-08-06 08:57:39 +0200

    add function verification\views.py\email_1

--- email_1/views.py
+++ email_1/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# Task 3
+import re
+from django.http import HttpResponse
 
-# Create your views here.
+
+def verification(request):
+    pattern = re.compile(r'^(?!.*--)(?!-)[a-zA-Z][a-zA-Z0-9_-]*[a-zA-Z0-9]@gmail\.com$')
+    email_2 = input("Please enter your email address: ")
+    matches = pattern.findall(email_2)
+
+    if len(matches) > 0:
+        return HttpResponse("your email address is correct :)")
+    else:
+        return HttpResponse("no correct email")
