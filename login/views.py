bd764f1663d2177a7ce64811b9b737321d41c65d
jevhen123zavirukha <jevhenzavirukha@gmail.com>
2024-08-06 08:59:04 +0200

    add function login_verification\views.py\login

--- login/views.py
+++ login/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# Task 4
+from django.http import HttpResponse
+import re
 
-# Create your views here.
+
+def login_verification(request):
+    pattern = re.compile("^[a-zA-Z0-9]{2,10}$")
+    login_2 = input("Please enter your password(login): ")
+
+    matches = pattern.findall(login_2)
+
+    if len(matches) > 0:
+        return HttpResponse("your login is correct :)")
+    else:
+        return HttpResponse("no correct login")
